3ae35bc87e2a227785a13762798099de201403ef
Scriptix <scriptixru@gmail.com>
2017-11-25 23:44:06 +0300

    69 Using Trigger Volumes

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -20,14 +20,18 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
+
+	
+}
+
+void UOpenDoor::OpenDoor()
+{
 	// Find the owning Actor
 	AActor* Owner = GetOwner();
 	// Create a rotator
 	FRotator NewRotation = FRotator(0.f, -60.0f, 0.f);
 	// Set the door rotation
 	Owner->SetActorRotation(NewRotation);
-
-	
 }
 
 
@@ -36,6 +40,13 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	//Poll the Trigger Volume 
+	//If the ActorThatOpen is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
+	
+	
 }
 
