e32785a906146afddc8118d1d2217c281f423fd1
Scriptix <scriptixru@gmail.com>
2017-11-25 22:03:02 +0300

    68 Macros Starting with UPROPERTY

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Runtime/Engine/Classes/Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -23,7 +25,12 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
 
-		
-	
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+
 };
