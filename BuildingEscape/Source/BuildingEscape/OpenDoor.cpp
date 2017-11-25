467817f5c3b34599f28d79d60fb7cba44b1c540f
Scriptix <scriptixru@gmail.com>
2017-11-25 19:55:10 +0300

    66 Laying Out Geometry

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -23,7 +23,7 @@ void UOpenDoor::BeginPlay()
 	// Find the owning Actor
 	AActor* Owner = GetOwner();
 	// Create a rotator
-	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+	FRotator NewRotation = FRotator(0.f, -60.0f, 0.f);
 	// Set the door rotation
 	Owner->SetActorRotation(NewRotation);
 
